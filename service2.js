5b48ef81e331b1a6e5a78b0a8f80ad5aca1d3448
Jared M. Peterson <jared@peteyinc.com>
2016-05-29 17:18:21 -0600

    s2 updates

--- service2.js
+++ service2.js
@@ -1,68 +1,67 @@
-angular.module('app').service('introvertSchedulingService', function($q, $timeout){
-  
-  this.willHangOut = function(){
-     
-    var deferObj = $q.defer();
-    var warm = false;
-    var date = false;
-    var distance = false;
-    
-    var warmDone = false;
-    var dateDone = false;
-    var distanceDone = false;
-    
-    
-    function checkWeather(){
-        $http.get('http://weather.com/api/warm')
-            .then(function(response){
-                warm = response.data;
-                warmDone = true;
-                checkAllGood();
-            })
-    }
-    
-    function checkHowLongSinceLastSocialFunction(){
-        $http.get('http://calendar.com/api/dateSinceLastDoingSomething')
-            .then(function(response){
-                date = response.data;
-                dateDone = true;
-                checkAllGood();
-            })
-    }
-    
-    function checkDistanceToTravel(){
-        $http.get('http://maps.com/api/notTooFarToDrive')
-            .then(function(response){
-                distance = response.data;
-                distanceDone = true;
-                checkAllGood();
-            })
-    }
-    
-    function checkAllGood() {
-        if(warmDone && dateDone && distanceDone){
-            if(warm && date && distance){
-                deferObj.resolve(true);
-            } else {
-                deferObj.reject(true);
+angular.module('app').service('introvertSchedulingService', function($q, $timeout) {
+
+    this.willHangOut = function() {
+        var deferObj = $q.defer();
+        var warm = false;
+        var date = false;
+        var distance = false;
+
+        var warmDone = false;
+        var dateDone = false;
+        var distanceDone = false;
+
+
+        function checkWeather() {
+            $http.get('http://weather.com/api/warm')
+                .then(function(response) {
+                    warm = response.data;
+                    warmDone = true;
+                    checkAllGood();
+                })
+        }
+
+        function checkHowLongSinceLastSocialFunction() {
+            $http.get('http://calendar.com/api/dateSinceLastDoingSomething')
+                .then(function(response) {
+                    date = response.data;
+                    dateDone = true;
+                    checkAllGood();
+                })
+        }
+
+        function checkDistanceToTravel() {
+            $http.get('http://maps.com/api/notTooFarToDrive')
+                .then(function(response) {
+                    distance = response.data;
+                    distanceDone = true;
+                    checkAllGood();
+                })
+        }
+
+        function checkAllGood() {
+            if (warmDone && dateDone && distanceDone) {
+                if (warm && date && distance) {
+                    deferObj.resolve(true);
+                } else {
+                    deferObj.reject(true);
+                }
             }
         }
+
+        checkDistanceToTravel();
+        checkHowLongSinceLastSocialFunction();
+        checkWeather();
+
+        return deferObj.promise;
     }
-    
-    checkDistanceToTravel();
-    checkHowLongSinceLastSocialFunction();
-    checkWeather();
-    
-    return deferObj.promise;
-  }
-  
+
 });
 
-angular.module('app').service('factorialService', function($q, $timeout){
-    
+angular.module('app').service('factorialService', function($q, $timeout) {
+
     //Look into promise chaining;
-    
-    this.doFactorial = function (startNum) {
+
+    this.doFactorial = function(startNum) {
         var defer = $q.defer();
 
         defer.resolve({
